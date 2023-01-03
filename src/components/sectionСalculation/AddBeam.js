0139144318ebf73eaf72d0d9c964f1b66772f58b
Falsin <greenldend@gmail.com>
2023-01-03 15:04:10 +0700

    сохранил все параметры фигур в замыкании

--- src/components/sectionСalculation/AddBeam.js
+++ src/components/sectionСalculation/AddBeam.js
@@ -1,33 +1,44 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-function AddBeam({ draw, className, children }) {
+function AddBeam({ saveShape, className, children }) {
   const [height, setHeight] = useState(100);
   const [width, setWidth] = useState(55);
   const [s, setS] = useState(4.1);
   const [t, setT] = useState(5.7);
+  const [coordX, setCoordX] = useState(0);
+  const [coordY, setCoordY] = useState(0);
 
-  function drawBeam(ctx, currentX, currentY) {
-    ctx.save();
-    ctx.translate(-width/2, -height/2);
+  function drawBeam(height, width, s, t, coordX, coordY) {
+    return function (ctx) {
+      if (ctx === undefined) {
+        return { height, width, s, t, coordX, coordY }
+      }
 
-    ctx.beginPath();
-    ctx.moveTo(currentX, currentY);
-    ctx.lineTo(currentX += width, currentY);
-    ctx.lineTo(currentX, currentY += t);
-    ctx.lineTo(currentX -= (width - s)/2, currentY);
-    ctx.lineTo(currentX, currentY += (height - 2*t));
-    ctx.lineTo(currentX += (width - s)/2, currentY);
-    ctx.lineTo(currentX, currentY += t);
-    ctx.lineTo(currentX -= width, currentY);
-    ctx.lineTo(currentX, currentY -= t);
-    ctx.lineTo(currentX += (width - s)/2, currentY);
-    ctx.lineTo(currentX, currentY -= (height - 2*t));
-    ctx.lineTo(currentX -= (width - s)/2, currentY);
-    ctx.closePath();
-    ctx.stroke();
+      let currentX = coordX;
+      let currentY = coordY;
 
-    ctx.restore();
+      ctx.save();
+      ctx.translate(-width/2, -height/2);
+
+      ctx.beginPath();
+      ctx.moveTo(currentX, currentY);
+      ctx.lineTo(currentX += width, currentY);
+      ctx.lineTo(currentX, currentY += t);
+      ctx.lineTo(currentX -= (width - s)/2, currentY);
+      ctx.lineTo(currentX, currentY += (height - 2*t));
+      ctx.lineTo(currentX += (width - s)/2, currentY);
+      ctx.lineTo(currentX, currentY += t);
+      ctx.lineTo(currentX -= width, currentY);
+      ctx.lineTo(currentX, currentY -= t);
+      ctx.lineTo(currentX += (width - s)/2, currentY);
+      ctx.lineTo(currentX, currentY -= (height - 2*t));
+      ctx.lineTo(currentX -= (width - s)/2, currentY);
+      ctx.closePath();
+      ctx.stroke();
+
+      ctx.restore();
+    }
   }
 
   function convertToNumber(e, setState) {
@@ -50,14 +61,24 @@ function AddBeam({ draw, className, children }) {
 
       <label htmlFor="beamT">Толщина полки (t):</label>
       <input id="beamT" onChange={(e) => convertToNumber(e, setT)} /* value={t} */ defaultValue={5.7} />
+
+      <div>
+        <p>Координаты</p>
+        <label>x <input value={coordX} onChange={(e) => convertToNumber(e, setCoordX)} /></label>
+        <label>y <input value={coordY} onChange={(e) => convertToNumber(e, setCoordY)} /></label>
+      </div>
         
-      <input type="button" value="Добавить" onClick={() => draw(drawBeam)} />
+      <input type="button" value="Добавить" onClick={() => saveShape(drawBeam(height, width, s, t, coordX, coordY))} />
     </li>
   )
 }
 
 export const StyledAddBeam = styled(AddBeam)`
-  label, input {
+  & > label, & > input {
     display: block;
+  }
+
+  div input {
+    width: 40px;
   }
 `

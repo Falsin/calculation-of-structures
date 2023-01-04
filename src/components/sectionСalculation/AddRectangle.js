2a655fd6fd141345a0b97fb04a2195c16de5ce3b
Falsin <greenldend@gmail.com>
2023-01-04 14:45:15 +0700

    Добавил возможность установку кординат и замыкание в функцию drawRectangle

--- src/components/sectionСalculation/AddRectangle.js
+++ src/components/sectionСalculation/AddRectangle.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-function AddRectangle({ draw, className, children }) {
+function AddRectangle({ saveShape, className, children }) {
   const [height, setHeight] = useState(100);
   const [width, setWidth] = useState(50);
+  const [coordX, setCoordX] = useState(0);
+  const [coordY, setCoordY] = useState(0);
 
-  function drawRectangle(ctx, currentX, currentY) {
-    ctx.save();
+  function drawRectangle(width, height, coordX, coordY) {
+    return function (ctx) {
+      if (ctx === undefined) {
+        return { width, height, coordX, coordY }
+      }
 
-    ctx.translate(-width/2, -height/2);
-    ctx.strokeRect(currentX, currentY, width, height);
+      let currentX = coordX;
+      let currentY = coordY;
 
-    ctx.fillText(`(${currentX}, ${currentY})`, currentX-10, currentY-5);
-    ctx.fillText(`(${currentX+width}, ${currentY})`, currentX+width-10, currentY-5);
-    ctx.fillText(`(${currentX}, ${currentY+height})`, currentX-10, currentY+height+10);
-    ctx.fillText(`(${currentX+width}, ${currentY+height})`, currentX+width-15, currentY+height+10);
+      ctx.save();
 
-    ctx.restore();
+      ctx.translate(-width/2, -height/2);
+      ctx.strokeRect(currentX, currentY, width, height);
+
+      ctx.fillText(`(${currentX}, ${currentY})`, currentX-10, currentY-5);
+      ctx.fillText(`(${currentX+width}, ${currentY})`, currentX+width-10, currentY-5);
+      ctx.fillText(`(${currentX}, ${currentY+height})`, currentX-10, currentY+height+10);
+      ctx.fillText(`(${currentX+width}, ${currentY+height})`, currentX+width-15, currentY+height+10);
+
+      ctx.restore();
+    }
   }
 
   function convertToNumber(e, setState) {
@@ -34,7 +45,13 @@ function AddRectangle({ draw, className, children }) {
       <label htmlFor="rectangleWidth">Ширина (b):</label>
       <input id="rectangleWidth" onChange={(e) => convertToNumber(e, setWidth)} /* value={width} */ defaultValue={50} />
         
-      <input type="button" value="Добавить" onClick={() => draw(drawRectangle)} />
+      <div>
+        <p>Координаты</p>
+        <label>x <input value={coordX} onChange={(e) => convertToNumber(e, setCoordX)} /></label>
+        <label>y <input value={coordY} onChange={(e) => convertToNumber(e, setCoordY)} /></label>
+      </div>
+
+      <input type="button" value="Добавить" onClick={() => saveShape(drawRectangle(width, height, coordX, coordY))} />
     </li>
   )
 }

17f92cb831a9eeb4bbbd4a837333cceeed497812
Falsin <greenldend@gmail.com>
2023-01-09 19:18:02 +0700

    добавил redux

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import { createRoot } from "react-dom/client";
 import { StyledSectionСalculation } from './components/sectionСalculation/SectionСalculation';
 //import SectionСalculation from './components/sectionСalculation/SectionСalculation';
 
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
 const root = createRoot(document.getElementById("root"))
-root.render(<StyledSectionСalculation />)
+root.render(
+  <Provider store={store}>
+    <StyledSectionСalculation />
+  </Provider>
+  )

c7d55f1b2516b75763dc08070981a9033331ed02
Falsin <greenldend@gmail.com>
2023-01-13 19:51:24 +0700

    Добавил селекторы для поиска двутавров и уголков. Добавил asyn thunk для запроса на сервер. Изменил исходное состояние storage и добавил extraReducers. В будущем удалю обычные reducers

--- src/redux/sortamentSlice.js
+++ src/redux/sortamentSlice.js
@@ -1,19 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const sortamentSlice = createSlice({
-  name: "sortament",
-  initialState: {
+const initialState = {
+  shapes: {
     beams: [],
     corners: []
   },
+  status: "idle",
+  error: null
+}
+
+export const sortamentSlice = createSlice({
+  name: "sortament",
+  initialState,
   reducers: {
-    saveBeams: (state, action) => {
-      state.beams = action.payload
-    },
+    /* saveBeams: (state, action) => {
+      state.shapes.beams = action.payload
+    }, */
     saveCorners: (state, action) => {
-      state.corners = action.payload
+      state.shapes.corners = action.payload
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBeams.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchBeams.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.shapes.beams = JSON.parse(action.payload);
+      })
+      .addCase(fetchBeams.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
   }
+})
+
+export const selectAllBeams = state => state.sortament.shapes.beams;
+export const selectAllCorners = state => state.sortament.shapes.corners;
+
+export const fetchBeams = createAsyncThunk("beams/fetchBeams", async () => {
+  const request = await fetch("http://localhost:3000/beams/");
+  const response = await request.json();
+  return response;
 })
 
 export const { saveBeams, saveCorners } = sortamentSlice.actions;

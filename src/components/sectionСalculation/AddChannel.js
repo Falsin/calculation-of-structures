56e772b542a536102b25d15f1ff85071aadb72a9
Falsin <greenldend@gmail.com>
2023-01-17 17:29:08 +0700

    сделал такой же функционал как и в addBeam

--- src/components/sectionСalculation/AddChannel.js
+++ src/components/sectionСalculation/AddChannel.js
@@ -1,39 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchShapes, selectAllChannels } from "../../redux/sortamentSlice";
 import styled from "styled-components";
 
 function AddChannel({ saveShape, className, children }) {
-  const [height, setHeight] = useState(50);
-  const [width, setWidth] = useState(32);
-  const [s, setS] = useState(4.4);
-  const [t, setT] = useState(7);
   const [coordX, setCoordX] = useState(0);
   const [coordY, setCoordY] = useState(0);
+  const [channel, setChannel] = useState(null);
 
-  function drawChannel(height, width, s, t, coordX, coordY) {
-    return function (ctx) {
-      if (ctx === undefined) {
-        return { height, width, s, t, coordX, coordY }
+  const channels = useSelector(selectAllChannels);
+  const status = useSelector(state => state.sortament.status);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchShapes("channels"))
+    }
+  })
+
+  function drawChannel(channel, coordX, coordY) {
+    const { h, b, s, t } = channel;
+
+    return function (svg, startPointX, startPointY) {
+      if (svg === undefined) {
+        return { ...channel, coordX, coordY, type: "channel" }
       }
 
-      let currentX = coordX;
-      let currentY = coordY;
+      const xmlns = "http://www.w3.org/2000/svg";
+      const currentX = startPointX + coordX;
+      const currentY = startPointY + coordY;
 
-      ctx.save();
-      ctx.translate(-width/2, -height/2);
-
-      ctx.beginPath();
-      ctx.moveTo(currentX, currentY);
-      ctx.lineTo(currentX += width, currentY);
-      ctx.lineTo(currentX, currentY += t);
-      ctx.lineTo(currentX -= width-s, currentY);
-      ctx.lineTo(currentX, currentY += height-2*t);
-      ctx.lineTo(currentX += width-s, currentY);
-      ctx.lineTo(currentX, currentY += t);
-      ctx.lineTo(currentX -= width, currentY);
-      ctx.closePath();
-      ctx.stroke();
-
-      ctx.restore();
+      const path = document.createElementNS(xmlns, "path");
+      path.setAttributeNS(null, "d", `M ${currentX}, ${currentY} h ${b} v ${t} h -${b - s} v ${h-2*t} h ${b - s} v ${t} h -${b}  z`)
+      path.setAttributeNS(null, "fill", "white");
+      path.setAttributeNS(null, "stroke", "black");
+      path.setAttributeNS(null, "transform", `translate(-${b/2}, -${h/2})`);
+    
+      svg.current.appendChild(path);
     }
   }
 
@@ -46,17 +49,14 @@ function AddChannel({ saveShape, className, children }) {
     <li className={className}>
       <p>Швеллер</p>
 
-      <label htmlFor="channelHeight">Высота (h):</label>
-      <input id="channelHeight" onChange={(e) => convertToNumber(e, setHeight)} /* value={height} */ defaultValue={50} />
-
-      <label htmlFor="channelWidth">Ширина полки (b):</label>
-      <input id="channelWidth" onChange={(e) => convertToNumber(e, setWidth)} /* value={width} */ defaultValue={50} />
-
-      <label htmlFor="channelS">Толщина стенки (s):</label>
-      <input id="channelS" onChange={(e) => convertToNumber(e, setS)} /* value={s} */ defaultValue={50} />
-
-      <label htmlFor="channelT">Толщина полки (t):</label>
-      <input id="channelT" onChange={(e) => convertToNumber(e, setT)} /* value={t} */ defaultValue={50} />
+      <select onChange={(e) => {
+        const channel = channels.find(channel => channel._id === e.target.value);
+        setChannel(channel);
+      }}>
+        <option>Выберите № швеллера</option>
+        {console.log(channels)}
+        {channels.map(channel => <option value={channel._id} key={channel._id}>{channel.no}</option>)}
+      </select>
         
       <div>
         <p>Координаты</p>
@@ -64,13 +64,17 @@ function AddChannel({ saveShape, className, children }) {
         <label>y <input value={coordY} onChange={(e) => convertToNumber(e, setCoordY)} /></label>
       </div>
 
-      <input type="button" value="Добавить" onClick={() => saveShape(drawChannel(height, width, s, t, coordX, coordY))} />
+      <input type="button" value="Добавить" onClick={() => saveShape(drawChannel(channel, coordX, coordY))} />
     </li>
   )
 }
 
 export const StyledAddChannel = styled(AddChannel)`
-  label, input {
+  & > label, & > input {
     display: block;
+  }
+
+  div input {
+    width: 40px;
   }
 `

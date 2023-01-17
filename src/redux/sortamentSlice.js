03e93c0aca15fa2763e00b38d862af9862bf7e82
Falsin <greenldend@gmail.com>
2023-01-17 17:30:09 +0700

    сделал универсальную функцию fetchShapes

--- src/redux/sortamentSlice.js
+++ src/redux/sortamentSlice.js
@@ -3,7 +3,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 const initialState = {
   shapes: {
     beams: [],
-    corners: []
+    channels: []
   },
   status: "idle",
   error: null
@@ -12,24 +12,17 @@ const initialState = {
 export const sortamentSlice = createSlice({
   name: "sortament",
   initialState,
-  reducers: {
-    /* saveBeams: (state, action) => {
-      state.shapes.beams = action.payload
-    }, */
-    saveCorners: (state, action) => {
-      state.shapes.corners = action.payload
-    }
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchBeams.pending, (state, action) => {
+      .addCase(fetchShapes.pending, (state, action) => {
         state.status = "loading";
       })
-      .addCase(fetchBeams.fulfilled, (state, action) => {
+      .addCase(fetchShapes.fulfilled, (state, action) => {
         state.status = "succeeded";
-        state.shapes.beams = JSON.parse(action.payload);
+        state.shapes[action.meta.arg] = JSON.parse(action.payload);
       })
-      .addCase(fetchBeams.rejected, (state, action) => {
+      .addCase(fetchShapes.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
       })
@@ -37,12 +30,11 @@ export const sortamentSlice = createSlice({
 })
 
 export const selectAllBeams = state => state.sortament.shapes.beams;
-export const selectAllCorners = state => state.sortament.shapes.corners;
+export const selectAllChannels = state => state.sortament.shapes.channels;
 
-export const fetchBeams = createAsyncThunk("beams/fetchBeams", async () => {
-  const request = await fetch("http://localhost:3000/beams/");
-  const response = await request.json();
-  return response;
+export const fetchShapes = createAsyncThunk("sortament/fetchShapes", async (shape) => {
+  const request = await fetch(`http://localhost:3000/${shape}/`);
+  return await request.json();
 })
 
 export const { saveBeams, saveCorners } = sortamentSlice.actions;

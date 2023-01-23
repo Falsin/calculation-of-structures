53cd913c7f4dcf58f1410a5a0a864712b685a750
Falsin <greenldend@gmail.com>
2023-01-23 18:37:26 +0700

    создал шаблонный slice creator

---
+++ src/redux/templateSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const shapeSlice = (shapeName, fetchFunc) => {
+  const initialState = { 
+    [shapeName]: [], 
+    status: "idle",
+    error: null 
+  }
+
+  return createSlice({
+    name: shapeName,
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchFunc.pending, (state) => {
+          state.status = "loading";
+        })
+        .addCase(fetchFunc.fulfilled, (state, action) => {
+          state.status = "succeeded";
+          state[shapeName] = JSON.parse(action.payload);
+        })
+        .addCase(fetchFunc.rejected, (state, action) => {
+          state.status = "failed";
+          state.error = action.error.message;
+        })
+    }
+  })
+}
+
+export const fetchShapes = (type, shape) => {
+  return createAsyncThunk(type, async () => {
+    const request = await fetch(`http://localhost:3000/${shape}/`);
+    return await request.json();
+  })
+}

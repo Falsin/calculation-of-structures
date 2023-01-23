658ac7d29fb999630e8de33e1be866f045e36346
Falsin <greenldend@gmail.com>
2023-01-23 18:36:56 +0700

    добавил beams reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sortamentReducer from "./sortamentSlice";
+import beamsReducer from "./beamsSlice";
 
 
 export default configureStore({
   reducer: {
-    sortament: sortamentReducer
+    sortament: sortamentReducer,
+    beams: beamsReducer,
   },
 })

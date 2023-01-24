e55f86f227dd12fbcffc629f5878edc111b252ce
Falsin <greenldend@gmail.com>
2023-01-24 20:39:34 +0700

    добавил все slice для каждого вида сечения

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import sortamentReducer from "./sortamentSlice";
 import beamsReducer from "./beamsSlice";
-
+import channelsReducer from "./channelsSlice";
+import equalAnglesCornersReducer from "./equalAngleCornerSlice";
+import unequalAnglesCornersReducer from "./unequalAnglesSlice";
 
 export default configureStore({
   reducer: {
-    sortament: sortamentReducer,
     beams: beamsReducer,
+    channels: channelsReducer,
+    equalAnglesCorners: equalAnglesCornersReducer,
+    unequalAnglesCorners: unequalAnglesCornersReducer,
   },
 })

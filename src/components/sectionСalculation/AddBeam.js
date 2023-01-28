4ec746d55b11cf34bde9935f00851a66e1df0cda
Falsin <greenldend@gmail.com>
2023-01-28 19:11:16 +0700

    Сделал так, что в компонент в качестве якорной точки передаётся центр сечения, а не самая крайняя левая точка. Соответственно изменил функционал

--- src/components/sectionСalculation/AddBeam.js
+++ src/components/sectionСalculation/AddBeam.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { fetchBeams, selectAllBeams } from "../../redux/beamsSlice";
@@ -22,65 +22,65 @@ function AddBeam({ saveShape, className, children }) {
   function drawBeam(beam, coordX, coordY, degree) {
     const {h, b, s, t} = beam;
 
-    return function (svg, startPointX, startPointY) {
+    return function (svg, centerX, centerY) {
       if (svg === undefined) {
         return {...beam, coordX, coordY, type: "beam", degree}
       }
 
       const xmlns = "http://www.w3.org/2000/svg";
-      const currentX = startPointX + coordX;
-      const currentY = startPointY + coordY;
 
       const path = document.createElementNS(xmlns, "path");
-      path.setAttributeNS(null, "d", `M ${currentX}, ${currentY} h ${b} v ${t} h -${(b - s)/2} v ${h-2*t} h ${(b - s)/2} v ${t} h -${b} v -${t} h ${(b - s)/2} v -${h - 2*t} h -${(b - s)/2} z`)
+      path.setAttributeNS(null, "d", `M ${centerX - b/2}, ${centerY - h/2} h ${b} v ${t} h -${(b - s)/2} v ${h-2*t} h ${(b - s)/2} v ${t} h -${b} v -${t} h ${(b - s)/2} v -${h - 2*t} h -${(b - s)/2} z`)
       path.setAttributeNS(null, "fill", "white");
       path.setAttributeNS(null, "stroke", "black");
       path.setAttributeNS(null, "id", "beam");
-
-      if (degree == 90) {
-        path.setAttributeNS(null, "transform", `rotate(-90, ${currentX}, ${currentY}) translate(${-b}, ${0})`);
-      }
+      path.setAttributeNS(null, "transform", `rotate(${degree}, ${centerX}, ${centerY})`);
 
       svg.current.appendChild(path);
 
-      const coords = [
-        {x: coordX, y: coordY}, 
-        {x: coordX + b, y: coordY},
-        {x: coordX + b, y: coordY + h},
-        {x: coordX, y: coordY + h},
-      ]
+      let coords;
+      let startPointX;
+      let startPointY;
+
+      if (degree == 90 || degree == 90) {
+        coords = [
+          {x: coordX, y: coordY}, 
+          {x: coordX + h, y: coordY },
+          {x: coordX + h, y: coordY + b},
+          {x: coordX, y: coordY + b}
+        ]
+
+        startPointX = centerX - h/2;
+        startPointY = centerY - b/2;
+      } else {
+        coords = [
+          {x: coordX, y: coordY}, 
+          {x: coordX + b, y: coordY},
+          {x: coordX + b, y: coordY + h},
+          {x: coordX, y: coordY + h},
+        ]
+
+        startPointX = centerX - b/2;
+        startPointY = centerY - h/2;
+      }
 
       coords.forEach((item, id) => {
         const text = document.createElementNS(xmlns, "text");
-        text.setAttributeNS(null, "x", `${startPointX + item.x}`);
-        text.setAttributeNS(null, "y", `${startPointY + item.y}`);
+
         text.setAttributeNS(null, "font-size", "10px");
         text.setAttributeNS(null, "text-anchor", "middle");
+        text.setAttributeNS(null, "x", `${startPointX + item.x}`);
+        text.setAttributeNS(null, "y", `${startPointY + item.y}`);
 
-        if (degree == 90) {
-          if (id == 0 || id == 3) {
-            text.setAttributeNS(null, "transform", `rotate(-90, ${currentX}, ${currentY}) rotate(90, ${startPointX + item.x}, ${startPointY + item.y})`);
-            text.textContent = `(${item.x}, ${item.y})`;
-          } else if (id == 1 || id == 2) {
-            text.setAttributeNS(null, "transform", `rotate(-90, ${currentX}, ${currentY}) translate(${-2*b}, ${0}) rotate(90, ${startPointX + item.x}, ${startPointY + item.y})`);
-            text.textContent = `(${item.y}, ${item.x})`;
-          }
-        } else {
-          text.textContent = `(${item.x}, ${item.y})`;
-        }
+        text.textContent = `(${item.x}, ${item.y})`;
 
         svg.current.appendChild(text)
       })
     }
   }
 
-  function convertToNumber(e, setState) {
-    const value = e.target.value;
-    setState((value[value.length - 1] === ".") ? value : parseFloat(value))
-  }
-
   function changeOrientation() {
-    degree == 0 ? setDegree(90) : setDegree(0);
+    setDegree(degree == 270 ? 0 : degree + 90);
   }
 
   return (
@@ -97,14 +97,45 @@ function AddBeam({ saveShape, className, children }) {
 
       <div>
         <p>Координаты</p>
-        <label>x <input value={coordX} onChange={(e) => convertToNumber(e, setCoordX)} /></label>
-        <label>y <input value={coordY} onChange={(e) => convertToNumber(e, setCoordY)} /></label>
+        <label>x <input value={coordX} onChange={(e) => setCoordX(e.target.value)} /></label>
+        <label>y <input value={coordY} onChange={(e) => setCoordY(e.target.value)} /></label>
       </div>
 
       <button onClick={changeOrientation}>{degree == 0 ? "Повернуть на 90°" : "Повернуть на 90°"}</button>
+
+      <Preview degree={degree} />
         
-      <input type="button" value="Добавить" onClick={() => saveShape(drawBeam(beam, coordX, coordY, degree))} />
+      <input type="button" value="Добавить" onClick={() => saveShape(drawBeam(beam, parseFloat(coordX), parseFloat(coordY), degree))} />
     </li>
+  )
+}
+
+function Preview({degree}) {
+  const svg = useRef(null);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  
+  const beam = useSelector(state => state.beams.beams[0]);
+  const {h, b, s, t} = !beam ? {} : beam;
+
+  useEffect(() => {
+    const style = getComputedStyle(svg.current);
+    setWidth(parseFloat(style.width))
+    setHeight(parseFloat(style.height))
+  })
+
+  return (
+    <svg ref={svg}>
+      {!beam 
+        ? null 
+        : <path 
+          d={`M ${width/2 - b/2}, ${height/2 - h/2} h ${b} v ${t} h -${(b - s)/2} v ${h-2*t} h ${(b - s)/2} v ${t} h -${b} v -${t} h ${(b - s)/2} v -${h - 2*t} h -${(b - s)/2} z`} 
+          transform={`rotate(${degree}, ${width/2}, ${height/2})`}
+          fill="white" 
+          stroke="black"  
+        />
+      }
+    </svg>
   )
 }
 

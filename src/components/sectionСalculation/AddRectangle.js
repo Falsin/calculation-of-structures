aeeff83a42c6c1f114c7ee6c7abdadf970031831
Falsin <greenldend@gmail.com>
2023-01-31 18:17:14 +0700

    Настроил такую же функциональность, как и в других сечениях

--- src/components/sectionСalculation/AddRectangle.js
+++ src/components/sectionСalculation/AddRectangle.js
@@ -4,31 +4,35 @@ import styled from "styled-components";
 function AddRectangle({ saveShape, className, children }) {
   const [height, setHeight] = useState(100);
   const [width, setWidth] = useState(50);
-  const [coordX, setCoordX] = useState(0);
-  const [coordY, setCoordY] = useState(0);
+  const [centerX, setCenterX] = useState(0);
+  const [centerY, setCenterY] = useState(0);
 
   function drawRectangle(b, h, coordX, coordY) {
-    return function (svg, startPointX, startPointY) {
+    const rectangleInstance = {
+      height: parseFloat(height),
+      width: parseFloat(width),
+      centerX,
+      centerY,
+      type: "rectangle",
+    }
+
+    return function (svg, relativeCenterX, relativeCenterY) {
       if (svg === undefined) {
-        return { b, h, coordX, coordY, type: "rectangle" }
+        return rectangleInstance;
       }
 
       const xmlns = "http://www.w3.org/2000/svg";
-      const currentX = startPointX + coordX;
-      const currentY = startPointY + coordY;
+      const {height, width} = rectangleInstance
 
       const path = document.createElementNS(xmlns, "path");
-      path.setAttributeNS(null, "d", `M ${currentX}, ${currentY} h ${b} v ${h} h ${-b} z`);
+      path.setAttributeNS(null, "d", `M ${relativeCenterX - width/2}, ${relativeCenterY - height/2} h ${width} v ${height} h ${-width} z`);
       path.setAttributeNS(null, "fill", "white");
       path.setAttributeNS(null, "stroke", "black");
+      path.setAttributeNS(null, "transform-origin", `${relativeCenterX} ${relativeCenterY}`);
+      path.setAttributeNS(null, "transform", `scale(1 -1)`);
 
       svg.current.appendChild(path);
     }
-  }
-
-  function convertToNumber(e, setState) {
-    const value = e.target.value;
-    setState((value[value.length - 1] === ".") ? value : parseFloat(value))
   }
 
   return (
@@ -36,18 +40,18 @@ function AddRectangle({ saveShape, className, children }) {
       <p>Прямоугольное сечение</p>
 
       <label htmlFor="rectangleHeight">Высота (h):</label>
-      <input id="rectangleHeight" onChange={(e) => convertToNumber(e, setHeight)} /* value={height} */ defaultValue={100} />
+      <input id="rectangleHeight" onChange={(e) => setHeight(e.target.value)} value={height} />
 
       <label htmlFor="rectangleWidth">Ширина (b):</label>
-      <input id="rectangleWidth" onChange={(e) => convertToNumber(e, setWidth)} /* value={width} */ defaultValue={50} />
+      <input id="rectangleWidth" onChange={(e) => setWidth(e.target.value)} value={width} />
         
       <div>
         <p>Координаты</p>
-        <label>x <input value={coordX} onChange={(e) => convertToNumber(e, setCoordX)} /></label>
-        <label>y <input value={coordY} onChange={(e) => convertToNumber(e, setCoordY)} /></label>
+        <label>x <input value={centerX} onChange={(e) => setCenterX(e.target.value)} /></label>
+        <label>y <input value={centerY} onChange={(e) => setCenterY(e.target.value)} /></label>
       </div>
 
-      <input type="button" value="Добавить" onClick={() => saveShape(drawRectangle(width, height, coordX, coordY))} />
+      <input type="button" value="Добавить" onClick={() => saveShape(drawRectangle())} />
     </li>
   )
 }

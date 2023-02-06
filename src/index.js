a391db3d952e090169614a6586653a5e91b27ca8
Falsin <greenldend@gmail.com>
2023-02-06 19:18:58 +0700

    обернул приложение в контейнер MathJaxContent

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { StyledSectionСalculation } from './components/sectionСalculation/SectionСalculation';
-//import SectionСalculation from './components/sectionСalculation/SectionСalculation';
-
+import { MathJaxContext } from "better-react-mathjax";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
 const root = createRoot(document.getElementById("root"))
 root.render(
   <Provider store={store}>
-    <StyledSectionСalculation />
+    <MathJaxContext> 
+      <StyledSectionСalculation />
+    </MathJaxContext>
   </Provider>
   )

d24981fcd28418131bbeda1d6ccf72d91db92dd2
Falsin <greenldend@gmail.com>
2023-02-27 03:52:23 +0700

    произвёл рефакторинг компонента. Теперь рисуются контрольные точки

--- src/components/sectionСalculation/Preview.js
+++ src/components/sectionСalculation/Preview.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
-export default function Preview({ sectionName, degree, activeCase }) {
+export default function Preview({ sectionName, degree, activeCase, setIdCoordInArray, isRadBtnContrPointsActive }) {
   const svg = useRef(null);
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
@@ -11,7 +11,7 @@ export default function Preview({ sectionName, degree, activeCase }) {
     const reqElem = sectionArr.find(elem => (elem.h || elem.B || elem.b) >= 100);
     return reqElem
   });
-  const {B, h, b, s, t} = !section ? {} : section;
+  const {B, h, b, s, t, z0, x0, y0} = !section ? {} : section;
 
   const drawStringSections = {
     beams: `M ${width/2 - b/2}, ${height/2 - h/2} h ${b} v ${t} h -${(b - s)/2} v ${h-2*t} h ${(b - s)/2} v ${t} h -${b} v -${t} h ${(b - s)/2} v -${h - 2*t} h -${(b - s)/2} z`,
@@ -20,14 +20,70 @@ export default function Preview({ sectionName, degree, activeCase }) {
     unequalAnglesCorners: `M ${width/2 - b/2}, ${height/2 - B/2} h ${t} v ${B - t} h ${b - t} v ${t} h ${-b} z`
   }
 
+  const coords = {
+    beams: [
+      {x: -b/2, y: -h/2},
+      {x: b/2, y: -h/2},
+      {x: -b/2, y: h/2},
+      {x: b/2, y: h/2} 
+    ],
+    channels: [
+      {x: -b/2, y: -h/2},
+      {x: b/2, y: -h/2},
+      {x: -b/2, y: h/2},
+      {x: b/2, y: h/2}
+    ],
+    equalAnglesCorners: [
+      {x: -b/2, y: -b/2},
+      {x: -b/2, y: b/2},
+      {x: b/2, y: b/2}
+    ],
+    unequalAnglesCorners: [
+      [
+        {x: -b/2, y: -B/2},
+        {x: -b/2, y: B/2},
+        {x: b/2, y: B/2},
+      ],
+      [
+        {x: b/2, y: -B/2},
+        {x: b/2, y: B/2},
+        {x: -b/2, y: B/2},
+      ]
+    ]
+  }
+
   useEffect(() => {
     const style = getComputedStyle(svg.current);
     setWidth(parseFloat(style.width));
     setHeight(parseFloat(style.height));
+  }, [])
+
+  useEffect(() => {
+    const circles = svg.current.querySelectorAll("circle");
+    circles.forEach(elem => elem.remove());
+
+    if (section && isRadBtnContrPointsActive) {  
+      const arr = !activeCase ? coords[sectionName] : coords[sectionName][activeCase-1];
+
+      arr.forEach((elem, id) => {
+        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        circle.setAttributeNS(null, "cx", `${width/2 + elem.x}`);
+        circle.setAttributeNS(null, "cy", `${height/2 + elem.y}`);
+        circle.setAttributeNS(null, "r", `${4}`);
+        circle.setAttributeNS(null, "fill", "blue");
+        circle.setAttributeNS(null, "transform", `rotate(${degree}, ${width/2}, ${height/2})`);
+
+        circle.addEventListener("click", () => {
+          setIdCoordInArray(id)
+        })
+  
+        svg.current.appendChild(circle);
+      });
+    }
   })
 
   return (
-    <svg ref={svg} style={{display: "block"}}>
+    <svg ref={svg} style={{display: "block", maxHeight: "150px"}}>
       {!section 
         ? null 
         : <path 

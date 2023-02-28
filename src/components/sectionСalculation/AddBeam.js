5e9e1a4622a7c319f81b738914d863332ee11374
Falsin <greenldend@gmail.com>
2023-02-28 22:27:34 +0700

    сделал возможность добавлять фигуры по контрольным точкам. Реализовал два режима. Сделал отдельный компонент RadioFields

--- src/components/sectionСalculation/AddBeam.js
+++ src/components/sectionСalculation/AddBeam.js
@@ -6,12 +6,15 @@ import changeStatus from "../../javascript/changeStatusInList";
 import uniqid from 'uniqid';
 import { StyledSectionLi } from "./styledComponents";
 import Preview from "./Preview";
+import createCirclesInSvg from "../../javascript/addCirclesToSVG";
 
-export default function AddBeam({ saveShape }) {
+export default function AddBeam({saveShape, isPointsModeActive }) {
   const [centerX, setCenterX] = useState(0);
   const [centerY, setCenterY] = useState(0);
   const [beam, setBeam] = useState(null);
   const [degree, setDegree] = useState(0);
+  const [idCoordInArray, setIdCoordInArray] = useState(null);
+  const [isBtnPointsActive, setBtnPointsStatus] = useState(false);
 
   const beams = useSelector(selectAllBeams);
   const status = useSelector(state => state.beams.status);
@@ -23,7 +26,33 @@ export default function AddBeam({ saveShape }) {
     }
   }, [])
 
-  function drawBeam() {
+  useEffect(() => {
+    if (idCoordInArray !== null) {
+      drawShapeUsingPoints()
+    }
+  }, [idCoordInArray])
+
+  async function drawShapeUsingPoints() {
+    const shapeArr = saveShape();
+     
+    const result = await createCirclesInSvg(shapeArr);
+    console.log(result)
+    setCenterX(result.x);
+    setCenterY(result.y);
+    drawShape(result.x, result.y)
+  }
+
+  useEffect(() => {
+    if (!isBtnPointsActive && idCoordInArray !== null) {
+      setIdCoordInArray(null);
+      createCirclesInSvg([]);
+    }
+  }, [isBtnPointsActive])
+
+
+  const drawShape = (centerX, centerY) => saveShape(drawBeam(centerX, centerY))
+
+  function drawBeam(centerX, centerY) {
     const beamInstance = {
       ...beam,
       centerX: parseFloat(centerX), 
@@ -41,6 +70,26 @@ export default function AddBeam({ saveShape }) {
       const xmlns = "http://www.w3.org/2000/svg";
       const {h, b, s, t, degree} = beamInstance;
 
+      const coords = [
+        {x: -b/2, y: h/2},
+        {x: b/2, y: h/2},
+        {x: -b/2, y: -h/2},
+        {x: b/2, y: -h/2} 
+      ]
+
+      createCirclesInSvg.shapeCollectObj = {
+        [beamInstance.uniqid]: {
+          coordPoints: coords.map(elem => {
+            return {
+              x: relativeCenterX + elem.x,
+              y: relativeCenterY + elem.y,
+            }
+          }),
+          relativeCenterX,
+          relativeCenterY,
+        }
+      }
+
       const path = document.createElementNS(xmlns, "path");
       path.setAttributeNS(null, "d", `M ${relativeCenterX - b/2}, ${relativeCenterY - h/2} h ${b} v ${t} h -${(b - s)/2} v ${h-2*t} h ${(b - s)/2} v ${t} h -${b} v -${t} h ${(b - s)/2} v -${h - 2*t} h -${(b - s)/2} z`)
       path.setAttributeNS(null, "fill", "white");
@@ -48,17 +97,10 @@ export default function AddBeam({ saveShape }) {
       path.setAttributeNS(null, "transform-origin", `${relativeCenterX} ${relativeCenterY}`);
       path.setAttributeNS(null, "transform", `scale(1 -1) rotate(${degree})`);
       path.setAttributeNS(null, "id", `${beamInstance.uniqid}`);
-
+      
       svg.current.appendChild(path);
 
-      let coords = [
-        {x: -b/2, y: h/2},
-        {x: b/2, y: h/2},
-        {x: -b/2, y: -h/2},
-        {x: b/2, y: -h/2} 
-      ]
-
-      createTextCoords(arguments, coords, degree)
+      createTextCoords(arguments, coords, degree);
     }
   }
 
@@ -78,19 +120,67 @@ export default function AddBeam({ saveShape }) {
           <option>Выберите № двутавра</option>
           {beams.map(beam => <option value={beam._id} key={beam._id}>{beam.no}</option>)}
         </select>
+        
+        <RadioFields
+          saveShape={saveShape} 
+          drawShape={drawShape} 
+          isPointsModeActive={isPointsModeActive} 
+          setBtnPointsStatus={setBtnPointsStatus} 
+          centerX={centerX}
+          setCenterX={setCenterX}
+          centerY={centerY}
+          setCenterY={setCenterY}
+        />
+
+        <button type="button" onClick={changeOrientation}>{degree == 0 ? "Повернуть на 90°" : "Повернуть на 90°"}</button>
+
+        <Preview 
+          sectionName={"beams"} 
+          degree={degree} 
+          isBtnPointsActive={isBtnPointsActive} 
+          setIdCoordInArray={setIdCoordInArray}
+        />
+      </div>
+    </StyledSectionLi>
+  )
+}
+
+function RadioFields({ drawShape, setBtnPointsStatus, isPointsModeActive, centerX, setCenterX, centerY, setCenterY }) {
+  let id;
+
+  return (
+    <>
+      <div>
+        <label htmlFor={id = uniqid()}>Добавить по координатам центра тяжести</label>
+        <input 
+          type="radio" 
+          name="mode" 
+          id={id} 
+          readOnly 
+          defaultChecked={true} 
+          onChange={() => setBtnPointsStatus(false)}
+        />
 
         <div>
           <p>Координаты центра тяжести</p>
           <label>x <input value={centerX} onChange={(e) => setCenterX(e.target.value)} /></label>
           <label>y <input value={centerY} onChange={(e) => setCenterY(e.target.value)} /></label>
+              
+          <input type="button" value="Добавить" onClick={() => drawShape(centerX, centerY)} />
         </div>
-
-        <button type="button" onClick={changeOrientation}>{degree == 0 ? "Повернуть на 90°" : "Повернуть на 90°"}</button>
-
-        <Preview sectionName={"beams"} degree={degree} />
-          
-        <input type="button" value="Добавить" onClick={() => saveShape(drawBeam())} />
       </div>
-    </StyledSectionLi>
+
+      <div>
+        <label htmlFor={id = uniqid()}>Добавить по контрольным точкам</label>
+        <input 
+          type="radio" 
+          name="mode" 
+          id={id} 
+          readOnly 
+          disabled={!isPointsModeActive ? true : false}
+          onChange={() => setBtnPointsStatus(true)}
+        />
+      </div>
+    </>
   )
 }

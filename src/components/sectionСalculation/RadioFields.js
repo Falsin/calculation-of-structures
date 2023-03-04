fa7fccddc80f948956fa282a4c0e48773438b908
Falsin <greenldend@gmail.com>
2023-03-04 17:23:28 +0700

    создал отдельный файл для RadioFields

---
+++ src/components/sectionСalculation/RadioFields.js
@@ -0,0 +1,42 @@
+import React from "react";
+import uniqid from 'uniqid';
+
+export default function RadioFields({ drawShape, setBtnPointsStatus, isPointsModeActive, centerX, setCenterX, centerY, setCenterY }) {
+  let id;
+
+  return (
+    <>
+      <div>
+        <label htmlFor={id = uniqid()}>Добавить по координатам центра тяжести</label>
+        <input 
+          type="radio" 
+          name="mode" 
+          id={id} 
+          readOnly 
+          defaultChecked={true} 
+          onChange={() => setBtnPointsStatus(false)}
+        />
+
+        <div>
+          <p>Координаты центра тяжести</p>
+          <label>x <input value={centerX} onChange={(e) => setCenterX(e.target.value)} /></label>
+          <label>y <input value={centerY} onChange={(e) => setCenterY(e.target.value)} /></label>
+              
+          <input type="button" value="Добавить" onClick={() => drawShape(centerX, centerY)} />
+        </div>
+      </div>
+
+      <div>
+        <label htmlFor={id = uniqid()}>Добавить по контрольным точкам</label>
+        <input 
+          type="radio" 
+          name="mode" 
+          id={id} 
+          readOnly 
+          disabled={!isPointsModeActive ? true : false}
+          onChange={() => setBtnPointsStatus(true)}
+        />
+      </div>
+    </>
+  )
+}

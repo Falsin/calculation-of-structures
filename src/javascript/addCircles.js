38e06edc58a1f63c13c548bd839faeafc93015d1
Falsin <greenldend@gmail.com>
2023-03-07 20:02:05 +0700

    удалил перенёс функцию calcRotateCoords в отдельный файл

--- src/javascript/addCircles.js
+++ src/javascript/addCircles.js
@@ -1,3 +1,5 @@
+import calcRotateCoords from "./calcRotateCoords";
+
 export default function createCircles(arrayProps, array, degree) {
   const [svg, relativeCenterX, relativeCenterY] = arrayProps;
   const xmlns = "http://www.w3.org/2000/svg";
@@ -13,27 +15,4 @@ export default function createCircles(arrayProps, array, degree) {
   
     svg.current.appendChild(circle)
   })
-}
-
-function calcRotateCoords(item, relativeCenterX, relativeCenterY, degree) {
-  const triangleHypotenuse = Math.sqrt(item.y**2 + item.x**2);
-  const tgx = (item.y < 0 ? -item.y : item.y) / (item.x < 0 ? -item.x : item.x);
-  const arctg = Math.atan(tgx)*180/Math.PI;
-
-  let degreeFromStartPoint
-
-  if (relativeCenterX + item.x < relativeCenterX && relativeCenterY + item.y > relativeCenterY) {
-    degreeFromStartPoint = 180 - arctg;
-  } else if (relativeCenterX + item.x > relativeCenterX && relativeCenterY + item.y > relativeCenterY) {
-    degreeFromStartPoint = arctg;
-  } else if (relativeCenterX + item.x < relativeCenterX && relativeCenterY + item.y < relativeCenterY) {
-    degreeFromStartPoint = 180 + arctg;
-  } else if (relativeCenterX + item.x > relativeCenterX && relativeCenterY + item.y < relativeCenterY) {
-    degreeFromStartPoint = 360 - arctg;
-  }
-  const rotateDegree = degreeFromStartPoint - degree;
-  const rotateX = relativeCenterX + triangleHypotenuse*Math.cos(rotateDegree*Math.PI/180);
-  const rotateY = relativeCenterY + triangleHypotenuse*Math.sin(rotateDegree*Math.PI/180);
-
-  return {rotateX , rotateY}
 }

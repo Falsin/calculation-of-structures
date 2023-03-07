095d9e849906d5bfec2401a90882284fc135abb2
Falsin <greenldend@gmail.com>
2023-03-07 20:00:08 +0700

    вынес функцию для расчёта координат при повороте относительно центра тяжести сечения в отдельный файл

---
+++ src/javascript/calcRotateCoords.js
@@ -0,0 +1,22 @@
+export default function calcRotateCoords(item, relativeCenterX, relativeCenterY, degree) {
+  const triangleHypotenuse = Math.sqrt(item.y**2 + item.x**2);
+  const tgx = (item.y < 0 ? -item.y : item.y) / (item.x < 0 ? -item.x : item.x);
+  const arctg = Math.atan(tgx)*180/Math.PI;
+
+  let degreeFromStartPoint
+
+  if (relativeCenterX + item.x < relativeCenterX && relativeCenterY + item.y > relativeCenterY) {
+    degreeFromStartPoint = 180 - arctg;
+  } else if (relativeCenterX + item.x > relativeCenterX && relativeCenterY + item.y > relativeCenterY) {
+    degreeFromStartPoint = arctg;
+  } else if (relativeCenterX + item.x < relativeCenterX && relativeCenterY + item.y < relativeCenterY) {
+    degreeFromStartPoint = 180 + arctg;
+  } else if (relativeCenterX + item.x > relativeCenterX && relativeCenterY + item.y < relativeCenterY) {
+    degreeFromStartPoint = 360 - arctg;
+  }
+  const rotateDegree = degreeFromStartPoint - degree;
+  const rotateX = relativeCenterX + triangleHypotenuse*Math.cos(rotateDegree*Math.PI/180);
+  const rotateY = relativeCenterY + triangleHypotenuse*Math.sin(rotateDegree*Math.PI/180);
+
+  return {rotateX , rotateY}
+}

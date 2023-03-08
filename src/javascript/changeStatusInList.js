18c913916197d81ca8f99048a37a26bc435e7281
Falsin <greenldend@gmail.com>
2023-03-08 21:47:38 +0700

    теперь при удалении активного статуса у DOM элемета, удаляется активный статус у всех его потомков

--- src/javascript/changeStatusInList.js
+++ src/javascript/changeStatusInList.js
@@ -2,10 +2,16 @@ export default function changeStatus(e) {
   const parentElem = e.target.offsetParent;
   const activeElem = parentElem.querySelector("li > .active");
 
+  switchChildren(parentElem);
   if (activeElem == e.target) {
-    return e.currentTarget.classList.remove("active");
+    return activeElem.classList.remove("active");
   } else if (activeElem) {
     activeElem.classList.remove("active");
   }
   e.currentTarget.classList.add("active");
+}
+
+function switchChildren(parentElem) {
+  const activeChildren = [...parentElem.querySelectorAll(".active")];
+  activeChildren.forEach(elem => elem.classList.remove("active"))
 }

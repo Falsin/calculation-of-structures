c09c4c28e427ab0f5a77f8de54031b878693c8ee
Falsin <greenldend@gmail.com>
2023-03-26 16:37:07 +0700

    сделал плавный поворот для осей в Preview

--- src/components/sectionСalculation/Preview.js
+++ src/components/sectionСalculation/Preview.js
@@ -1,16 +1,61 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { createAxisArray, drawAxis, rotate } from "../../javascript/drawShapesArray";
 
 export default function Preview({ sectionName, degree, activeCase, setIdCoordInArray, isBtnPointsActive }) {
   const svg = useRef(null);
+  const g = useRef(null)
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
+  const [deg, setDeg] = useState(degree);
+  const [axisArr, setAxisArr] = useState([])
   
   const section = useSelector(state => {
     const sectionArr = state[sectionName][sectionName];
     const reqElem = sectionArr.find(elem => (elem.h || elem.B || elem.b) >= 100);
     return reqElem
   });
+
+  useEffect(() => {
+    if (degree !== deg) {
+      setDeg(deg + 90)
+    }
+  }, [degree]);
+
+  useEffect(() => {
+    if (section) {
+      const style = getComputedStyle(svg.current);
+
+      const dist = z0*10 || x0*10 || b/2;
+      let centerX = width/2 + (activeCase == 2 ? b/2 - dist : - b/2 + dist);
+
+      const centerY = height/2 - (B/2 || h/2 || b/2) + (h/2 || z0*10 || y0*10)
+
+      const arr = createAxisArray(style, centerX, centerY, 0).commonAxes
+        .filter(elem => elem.axisName != `Yсл` && elem.axisName != `Xсл`);
+      
+      setAxisArr(arr.map((axis, id) => drawAxis(axis, (id == 0) ? "red" : "green")))
+    }
+  }, [section]);
+
+  useEffect(() => {
+    axisArr.forEach(obj => {
+      Object.entries(obj)
+        .filter(([key]) => key != "text")
+        .forEach(([, val]) => val.setAttributeNS(null, "transform", `scale(${activeCase == 2 ? -1 : 1}, 1)`))
+    })
+  }, [activeCase])
+
+  useEffect(() => {
+    if (axisArr.length) {
+      axisArr.forEach(obj => {
+        Object.entries(obj)
+          .filter(([key]) => key != "text")
+          .forEach(([, value]) => g.current.appendChild(value));
+        obj.line.setAttributeNS(null, "transform-origin", `${width/2} ${height/2}`);
+      })
+    }
+  }, [axisArr])
 
   const {B, h, b, s, t, z0, x0, y0} = !section ? {} : section;
 
@@ -72,7 +117,7 @@ export default function Preview({ sectionName, degree, activeCase, setIdCoordInA
         circle.setAttributeNS(null, "cy", `${height/2 + elem.y}`);
         circle.setAttributeNS(null, "r", `${4}`);
         circle.setAttributeNS(null, "fill", "blue");
-        circle.setAttributeNS(null, "transform", `rotate(${-degree}, ${width/2}, ${height/2})`);
+        circle.setAttributeNS(null, "transform", `rotate(${-deg}, ${width/2}, ${height/2})`);
 
         circle.addEventListener("click", () => {
           setIdCoordInArray(id);
@@ -85,16 +130,18 @@ export default function Preview({ sectionName, degree, activeCase, setIdCoordInA
 
   return (
     <svg ref={svg} style={{display: "block", maxHeight: "150px", transform: "scale(1, -1)"}}>
-      {!section 
-        ? null 
-        : <path 
-          d={drawStringSections[sectionName]}
-          transform-origin="50% 50%"
-          transform={(activeCase == 2) ? `scale(-1 -1) rotate(${-degree})` : `scale(1 -1) rotate(${degree})`}
-          fill="white" 
-          stroke="black"
-        />
-      }
+      <g ref={g} style={{transform: `rotate(${-deg}deg)`, transformOrigin: "50% 50%"}}>
+        {!section 
+          ? null 
+          : <path 
+            d={drawStringSections[sectionName]}
+            transform-origin="50% 50%"
+            transform={`scale(${activeCase == 2 ? -1 : 1}, -1)`}
+            fill="white" 
+            stroke="black"
+          />
+        }
+      </g>
     </svg>
   )
 }

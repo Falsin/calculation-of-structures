c243de66dfecddcca8f1afac0dcb2b8e58ecf88e
Falsin <greenldend@gmail.com>
2023-03-30 23:47:01 +0700

    рефакторинг модуля: теперь координаты добавляются при помощи тега g который и совершает вращение

--- src/javascript/addCoordText.js
+++ src/javascript/addCoordText.js
@@ -1,44 +1,27 @@
 export default function createTextCoords(arrayProps, array, degree) {
   const [svg, relativeCenterX, relativeCenterY] = arrayProps;
-  const xmlns = "http://www.w3.org/2000/svg";
+
+  const style = getComputedStyle(svg.current);
+  const width = parseFloat(style.width);
+  const height = parseFloat(style.height);
+
+  const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  group.setAttributeNS(null, "transform", `rotate(${-degree})`);
+  group.setAttributeNS(null, "transform-origin", `${width/2} ${height/2}`);
 
   array.forEach(item => {
-    const text = document.createElementNS(xmlns, "text");
+    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
     text.setAttributeNS(null, "font-size", "10px");
     text.setAttributeNS(null, "text-anchor", "middle");
 
-    const {rotateX, rotateY} = calcRotateCoords(item, relativeCenterX, relativeCenterY, degree)
+    text.setAttributeNS(null, "transform-origin", `${relativeCenterX + item.x} ${relativeCenterY + item.y}`);
+    text.setAttributeNS(null, "transform", `scale(1 -1) rotate(${-degree})`);
+    text.setAttributeNS(null, "x", `${relativeCenterX + item.x}`);
+    text.setAttributeNS(null, "y", `${relativeCenterY + item.y}`);
+    text.textContent = `(${(item.x).toFixed(1)}, ${(item.y).toFixed(1)})`;
 
-    text.setAttributeNS(null, "transform-origin", `${rotateX} ${rotateY}`);
-    text.setAttributeNS(null, "transform", `scale(1 -1)`);
-
-    text.setAttributeNS(null, "x", `${rotateX}`);
-    text.setAttributeNS(null, "y", `${(rotateY.toFixed(1) > relativeCenterY) ? rotateY - 5 : rotateY+10}`);
-    text.textContent = `(${(rotateX-relativeCenterX).toFixed(1)}, ${(rotateY - relativeCenterY).toFixed(1)})`;
-
-    svg.current.appendChild(text)
+    group.appendChild(text);
   })
-}
 
-function calcRotateCoords(item, relativeCenterX, relativeCenterY, degree) {
-  const triangleHypotenuse = Math.sqrt(item.y**2 + item.x**2);
-    const tgx = (item.y < 0 ? -item.y : item.y) / (item.x < 0 ? -item.x : item.x);
-    const arctg = Math.atan(tgx)*180/Math.PI;
-
-    let degreeFromStartPoint
-
-    if (relativeCenterX + item.x < relativeCenterX && relativeCenterY + item.y > relativeCenterY) {
-      degreeFromStartPoint = 180 - arctg;
-    } else if (relativeCenterX + item.x > relativeCenterX && relativeCenterY + item.y > relativeCenterY) {
-      degreeFromStartPoint = arctg;
-    } else if (relativeCenterX + item.x < relativeCenterX && relativeCenterY + item.y < relativeCenterY) {
-      degreeFromStartPoint = 180 + arctg;
-    } else if (relativeCenterX + item.x > relativeCenterX && relativeCenterY + item.y < relativeCenterY) {
-      degreeFromStartPoint = 360 - arctg;
-    }
-    const rotateDegree = degreeFromStartPoint - degree;
-    const rotateX = relativeCenterX + triangleHypotenuse*Math.cos(rotateDegree*Math.PI/180);
-    const rotateY = relativeCenterY + triangleHypotenuse*Math.sin(rotateDegree*Math.PI/180);
-
-    return {rotateX , rotateY}
+  svg.current.appendChild(group);
 }

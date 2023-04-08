f6be8f395e5a8fe08eb98377b9c88d21d2a9874d
Falsin <greenldend@gmail.com>
2023-04-09 01:36:52 +0700

    перенёс функционал по созданю сечения и массива координат в класс Section

--- src/javascript/setSectionData.js
+++ src/javascript/setSectionData.js
@@ -1,69 +1,17 @@
-import uniqid from 'uniqid';
 import calcRotateCoords from "./calcRotateCoords";
+import Section from './Section';
 
 export default function setSectionData(centerX, centerY, degree, idCoordInArray, activeCase) {
-  const sectionInstance = {
-    ...this,
-    centerX: parseFloat(centerX), 
-    centerY: parseFloat(centerY),
-    degree,
-    uniqid: uniqid(),
-    isActive: false,
-  }
+  const sectionInstance = new Section(centerX, centerY, degree, this, activeCase);
 
-  const { h, B, b, s, t, z0, x0, y0 } = sectionInstance;
-
-  let coords; 
-
-  if (this.type == "beam") {
-    coords = [
-      {x: -b/2, y: h/2},
-      {x: b/2, y: h/2},
-      {x: b/2, y: -h/2},
-      {x: -b/2, y: -h/2} 
-    ]
-  } else if (this.type == "channel") {
-    coords = [
-      {x: -z0*10, y: h/2},
-      {x: b - z0*10, y: h/2},
-      {x: b - z0*10, y: -h/2},
-      {x: -z0*10, y: -h/2}
-    ]
-  } else if (this.type == "equalAnglesCorner") {
-    coords = [
-      {x: -z0*10, y: b - z0*10},
-      {x: b - z0*10, y: -z0*10},
-      {x: -z0*10, y: -z0*10}
-    ]
-  } else if (this.type == "unequalAnglesCorners") {
-    if (activeCase == 1) {
-      coords = [
-        {x: -x0*10, y: B - y0*10},
-        {x: b - x0*10, y: -y0*10},
-        {x: -x0*10, y: -y0*10}
-      ]
-    } else {
-      coords = [
-        {x: x0*10, y: B - y0*10},
-        {x: x0*10, y: -y0*10},
-        {x: -b + x0*10, y: -y0*10},
-      ]
-    }
-  } else {
-    coords = [
-      {x: -b/2, y: h/2},
-      {x: b/2, y: h/2},
-      {x: b/2, y: -h/2},
-      {x: -b/2, y: -h/2}
-    ]
-  }
+  sectionInstance.setCoords();
 
   const { rotateX = 0, rotateY = 0 } = (idCoordInArray !== null && idCoordInArray !== undefined)
-    ? calcRotateCoords(coords[idCoordInArray], 0, 0, degree) 
+    ? calcRotateCoords(sectionInstance.coords[idCoordInArray], 0, 0, degree) 
     : {}
 
   sectionInstance.centerX = centerX - rotateX;
-  sectionInstance.centerY = centerY - rotateY;          
+  sectionInstance.centerY = centerY - rotateY;
 
-  return { sectionInstance, coords }
+  return sectionInstance;
 }

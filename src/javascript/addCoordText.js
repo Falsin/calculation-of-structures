d3bc28f14b5b667418f33394202585c75431743c
Falsin <greenldend@gmail.com>
2023-04-08 20:15:27 +0700

    изменил параметры функции

--- src/javascript/addCoordText.js
+++ src/javascript/addCoordText.js
@@ -1,22 +1,20 @@
-/* import calcScale from "./calcScale"; */
-
-function createTextCoords(arrayProps, array, section, scale) {
-  const [svg, relativeCenterX, relativeCenterY] = arrayProps;
+function createTextCoords(obj, argFunc, scale) {
+  const [ svg ] = argFunc;
 
   const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
-  group.setAttributeNS(null, "transform", `rotate(${-section.degree})`);
-  group.setAttributeNS(null, "transform-origin", `${relativeCenterX} ${relativeCenterY}`);
+  group.setAttributeNS(null, "transform", `rotate(${-obj.sectionInstance.degree})`);
+  group.setAttributeNS(null, "transform-origin", `${obj.x} ${obj.y}`);
 
-  array.forEach(item => {
+  obj.coords.forEach(item => {
     const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
     text.setAttributeNS(null, "font-size", `${(16/scale)+2}`);
     text.setAttributeNS(null, "text-anchor", "middle");
 
-    text.setAttributeNS(null, "transform-origin", `${relativeCenterX + item.x} ${relativeCenterY + item.y}`);
-    text.setAttributeNS(null, "transform", `scale(1 -1) rotate(${-section.degree})`);
-    text.setAttributeNS(null, "x", `${relativeCenterX + item.x}`);
-    text.setAttributeNS(null, "y", `${relativeCenterY + item.y}`);
-    text.textContent = `(${(section.centerX + item.x).toFixed(1)}, ${(section.centerY + item.y).toFixed(1)})`;
+    text.setAttributeNS(null, "transform-origin", `${obj.x + item.x} ${obj.y + item.y}`);
+    text.setAttributeNS(null, "transform", `scale(1 -1) rotate(${-obj.sectionInstance.degree})`);
+    text.setAttributeNS(null, "x", `${obj.x + item.x}`);
+    text.setAttributeNS(null, "y", `${obj.y + item.y}`);
+    text.textContent = `(${(obj.x + item.x).toFixed(1)}, ${(obj.y + item.y).toFixed(1)})`;
 
     group.appendChild(text);
   })

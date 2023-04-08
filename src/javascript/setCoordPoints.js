a6cdca6754e0a939534e4cf05333789013d4b720
Falsin <greenldend@gmail.com>
2023-04-09 01:35:43 +0700

    изменил путь сохранения относительных координат

--- src/javascript/setCoordPoints.js
+++ src/javascript/setCoordPoints.js
@@ -1,12 +1,12 @@
 import createCirclesInSvg from "./addCirclesToSVG";
 
-export default function setCoordPoints(coords, valArr) {
+export default function setCoordPoints(valArr) {
   const [, relativeCenterX, relativeCenterY] = valArr;
 
   createCirclesInSvg.shapeCollectObj = {
     ...createCirclesInSvg.shapeCollectObj,
     [this.uniqid]: {
-      coordPoints: coords.map(elem => {
+      coordPoints: this.coords.map(elem => {
         return {
           x: relativeCenterX + elem.x,
           y: relativeCenterY + elem.y,

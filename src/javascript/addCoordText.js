45b8650e15e7be1c2c43fecb2856af7468e38974
Falsin <greenldend@gmail.com>
2023-04-09 01:32:49 +0700

    изменил путь к свойствам координат объекта

--- src/javascript/addCoordText.js
+++ src/javascript/addCoordText.js
@@ -5,7 +5,7 @@ function createTextCoords(obj, argFunc, scale) {
   group.setAttributeNS(null, "transform", `rotate(${-obj.sectionInstance.degree})`);
   group.setAttributeNS(null, "transform-origin", `${obj.x} ${obj.y}`);
 
-  obj.coords.forEach(item => {
+  obj.sectionInstance.coords.forEach(item => {
     const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
     text.setAttributeNS(null, "font-size", `${(16/scale)+2}`);
     text.setAttributeNS(null, "text-anchor", "middle");
@@ -14,7 +14,7 @@ function createTextCoords(obj, argFunc, scale) {
     text.setAttributeNS(null, "transform", `scale(1 -1) rotate(${-obj.sectionInstance.degree})`);
     text.setAttributeNS(null, "x", `${obj.x + item.x}`);
     text.setAttributeNS(null, "y", `${obj.y + item.y}`);
-    text.textContent = `(${(obj.x + item.x).toFixed(1)}, ${(obj.y + item.y).toFixed(1)})`;
+    text.textContent = `(${(obj.sectionInstance.centerX + item.x).toFixed(1)}, ${(obj.sectionInstance.centerY + item.y).toFixed(1)})`;
 
     group.appendChild(text);
   })

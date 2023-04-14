bfe1e651b3af985c1d567810c9147d9ecee07631
Falsin <greenldend@gmail.com>
2023-04-15 00:10:23 +0700

    создал отдельный компонент для рисования оси

---
+++ src/components/sectionСalculation/AxisComponent.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import uniqid from 'uniqid';
+
+export default function Axis({elem, scale, activeCase}) {
+  const [id] = useState(uniqid())
+
+  console.log('hello!')
+
+  return <g>
+    <defs>
+        <marker
+          id={id}
+          markerWidth="8" 
+          markerHeight="10"
+          refX="4"
+          refY="10"
+          stroke="black"
+          orient={`${elem.x1 == elem.x2 ? '0' : '-90'}`}
+        >
+            <path d="M 0.5, 0 l 3.5 10 l 3.5 -10"/>
+        </marker>   
+      </defs>
+
+      <path d={`M ${elem.x1} ${elem.y1} L ${elem.x2} ${elem.y2}`} markerEnd={`url(#${id})`}/>
+
+      <text
+        x={`${elem.x2}`}
+        y={`${elem.y2}`}
+        transform-origin={`${elem.x2} ${elem.y2}`}
+        textAnchor="middle"
+        fontSize={`${(16/scale)+2}`}
+        transform={`scale(${activeCase == 2 ? -1 : 1} -1)`}
+      >
+        {elem.axisName}
+      </text>
+  </g>
+}

96f253f14c288254054b3349ee1f75813b4093de
Falsin <greenldend@gmail.com>
2023-04-19 15:12:38 +0700

    изменил фунцию createCirclesInSvg. Теперь она просто записывает в своё свойство вычесленные координаты при повороте сечения и ничего не не возвращает

--- src/javascript/addCirclesToSVG.js
+++ src/javascript/addCirclesToSVG.js
@@ -1,41 +1,20 @@
 import calcRotateCoords from "./calcRotateCoords";
 
-export default function createCirclesInSvg(shapeArr) {
-  const circles = createCirclesInSvg.svg.querySelectorAll("circle");
-  circles.forEach(circle => circle.remove());
+export default function createCirclesInSvg(shape) {
+  if (!createCirclesInSvg.shapeCollectObj[shape.uniqid]) {
+    const changedArrayCoords =  shape.coords.map(obj => {
+      const rotateCoords = calcRotateCoords(obj, 0, 0, shape.degree);
+      rotateCoords.rotateX += shape.centerX;
+      rotateCoords.rotateY += shape.centerY;
 
-  return Promise.any(shapeArr.map(elem => {
-    return new Promise((res, rej) => {
-      const shape = elem();
-
-      const requireCoords = createCirclesInSvg.shapeCollectObj[shape.uniqid];
-    
-      requireCoords.coordPoints.forEach((item, id) => {
-        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        circle.setAttributeNS(null, "r", `${4}`);
-        circle.setAttributeNS(null, "fill", "blue");
-
-        const calcCoords = {
-          x: item.x - requireCoords.relativeCenterX,
-          y: item.y - requireCoords.relativeCenterY
-        }
-
-        const { rotateX, rotateY } = calcRotateCoords(calcCoords, requireCoords.relativeCenterX, requireCoords.relativeCenterY, shape.degree)
-        circle.setAttributeNS(null, "cx", `${rotateX}`);
-        circle.setAttributeNS(null, "cy", `${rotateY}`);
-
-        circle.addEventListener("click", () => {
-          res({
-            x: shape.centerX + (rotateX - requireCoords.relativeCenterX),
-            y: shape.centerY + (rotateY - requireCoords.relativeCenterY),
-          });
-        })
-  
-        createCirclesInSvg.svg.appendChild(circle);
-      })
+      return {x: rotateCoords.rotateX, y: rotateCoords.rotateY}
     })
-  }))
+
+    createCirclesInSvg.shapeCollectObj = {
+      ...createCirclesInSvg.shapeCollectObj,
+      [shape.uniqid]: changedArrayCoords
+    }
+  }
 }
 
-createCirclesInSvg.svg = null;
 createCirclesInSvg.shapeCollectObj = {};

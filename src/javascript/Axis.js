6a9705d926e08cabf657e1e7c4b8a2372784f64b
Falsin <greenldend@gmail.com>
2023-04-21 15:15:13 +0700

    применил оператор нулевого слияния

--- src/javascript/Axis.js
+++ src/javascript/Axis.js
@@ -1,9 +1,9 @@
 export default class Axis {
   constructor(obj, id, styleObj) {
-    this.x1 = obj.x || styleObj.x;
-    this.x2 = obj.x || styleObj.x+styleObj.width;
-    this.y1 = obj.y || styleObj.y;
-    this.y2 = obj.y || styleObj.y+styleObj.height;
+    this.x1 = obj.x ?? styleObj.x;
+    this.x2 = obj.x ?? styleObj.x+styleObj.width;
+    this.y1 = obj.y ?? styleObj.y;
+    this.y2 = obj.y ?? styleObj.y+styleObj.height;
     this.axisName = (id == undefined) ? null : (this.x1 == this.x2) ? `Y${id+1}` : `X${id+1}`
   }
 

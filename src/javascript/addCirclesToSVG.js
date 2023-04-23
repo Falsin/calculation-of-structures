60b62a6f642e5a7b1002e0c2246c56cb21cdb58f
Falsin <greenldend@gmail.com>
2023-04-23 14:09:53 +0700

    теперь вычисление координат контрольных точек при повороте происходит без проверки вхождения данного сечения в shapeCollectionObj

--- src/javascript/addCirclesToSVG.js
+++ src/javascript/addCirclesToSVG.js
@@ -1,19 +1,17 @@
 import calcRotateCoords from "./calcRotateCoords";
 
 export default function createCirclesInSvg(shape) {
-  if (!createCirclesInSvg.shapeCollectObj[shape.uniqid]) {
-    const changedArrayCoords =  shape.coords.map(obj => {
-      const rotateCoords = calcRotateCoords(obj, 0, 0, shape.degree);
-      rotateCoords.rotateX += shape.centerX;
-      rotateCoords.rotateY += shape.centerY;
+  const changedArrayCoords =  shape.coords.map(obj => {
+    const rotateCoords = calcRotateCoords(obj, 0, 0, shape.degree);
+    rotateCoords.rotateX += shape.centerX;
+    rotateCoords.rotateY += shape.centerY;
 
-      return {x: +(rotateCoords.rotateX).toFixed(1), y: +(rotateCoords.rotateY).toFixed(1)}
-    })
+    return {x: +(rotateCoords.rotateX).toFixed(1), y: +(rotateCoords.rotateY).toFixed(1)}
+  })
 
-    createCirclesInSvg.shapeCollectObj = {
-      ...createCirclesInSvg.shapeCollectObj,
-      [shape.uniqid]: changedArrayCoords
-    }
+  createCirclesInSvg.shapeCollectObj = {
+    ...createCirclesInSvg.shapeCollectObj,
+    [shape.uniqid]: changedArrayCoords
   }
 }
 

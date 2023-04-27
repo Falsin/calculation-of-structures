44e541f0c0a355c1b3fe3f209213321346316525
Falsin <greenldend@gmail.com>
2023-04-27 11:42:47 +0700

    добавил styledComponents для RadioFields и SVG

--- src/components/sectionСalculation/styledComponents.js
+++ src/components/sectionСalculation/styledComponents.js
@@ -4,7 +4,7 @@ const StyledSectionLi = styled.li`
   h3 {
     margin: 0;
 
-    & ~ div {
+   & ~ div {
       overflow: hidden;
       max-height: 0;
       padding: 0;
@@ -22,4 +22,34 @@ const StyledSectionLi = styled.li`
   }
 `
 
-export { StyledSectionLi };
+const RadioFields = styled.div`
+  & ~ div {
+    overflow: hidden;
+    max-height: 0;
+    padding: 0;
+    transition: 1s;
+  }
+
+  &.active ~ div {
+    max-height: 1000px;
+    margin-top: 10px;
+  }
+
+  div input {
+    width: 40px;
+  }
+`
+
+const SVG = styled.svg`
+  width: 800px;
+  height: 600px;
+  border: 1px solid black;
+  transform: scale(1, -1);
+
+  & g {
+    width: inherit;
+    height: inherit;
+  }
+`
+
+export { StyledSectionLi, RadioFields, SVG };

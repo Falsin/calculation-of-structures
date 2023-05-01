d125f67c890c22ec9b3d76a513d4c8907cb741c3
Falsin <greenldend@gmail.com>
2023-05-01 17:21:52 +0700

    добавил масшабирование. Теперь если height < 0, размер отступает вниз, иначе вверх

--- src/javascript/createDimensionalLine.js
+++ src/javascript/createDimensionalLine.js
@@ -1,4 +1,6 @@
-export default function createDimensionalLine (coordObj, length, degree, height, orientation) {
+import React from "react";
+
+export default function createDimensionalLine (coordObj, length, degree, height, scale ,orientation) {
   const { x, y } = coordObj;
 
   if (degree/360%1*360 == 90 || degree/360%1*360 == 270) {
@@ -8,17 +10,17 @@ export default function createDimensionalLine (coordObj, length, degree, height,
   return <g style={{transformOrigin: `${x}px ${y}px`, transform: `rotate(${(orientation == "vertical") ? "90deg" : "0"})`}}>
     <path d={`M ${x-2} ${y+height} l ${length+4} ${0}`}/>
 
-    <path d={`M ${x} ${y+2} l ${0} ${height}`}/>
-    <path d={`M ${x+length} ${y+2} l ${0} ${height}`}/>
+    <path d={`M ${x} ${y+(height > 0 ? 2 : -2)} l ${0} ${height}`}/>
+    <path d={`M ${x+length} ${y+(height > 0 ? 2 : -2)} l ${0} ${height}`}/>
 
-    <path d={`M ${x} ${y+8} l ${0} ${4}`} style={{transform: `rotate(-45deg)`, transformOrigin: `${x}px ${y+height}px`}}/>
-    <path d={`M ${x+length} ${y+8} l ${0} ${4}`} style={{transform: `rotate(-45deg)`, transformOrigin: `${x+length}px ${y+height}px`}}/>
+    <path d={`M ${x} ${y+(height-2)} l ${0} ${4}`} style={{transform: `rotate(-45deg)`, transformOrigin: `${x}px ${y+height}px`}}/>
+    <path d={`M ${x+length} ${y+(height-2)} l ${0} ${4}`} style={{transform: `rotate(-45deg)`, transformOrigin: `${x+length}px ${y+height}px`}}/>
 
     <g style={{transformOrigin: `${x+length/2}px ${y+height}px`, transform: `rotate(${degree}deg)`}}>
       <text 
-        style={{fontSize: `${(16/scale)+2}px`, transform: `scale(1, -1)`, transformOrigin: `${x+length/2}px ${y+12}px`}}
+        style={{fontSize: `${(16/scale)+2}px`, transform: `scale(1, -1)`, transformOrigin: `${x+length/2}px ${y+height+2}px`}}
         x={x+length/2} 
-        y={y+12}
+        y={y+height+2}
       >
         {length}
       </text>

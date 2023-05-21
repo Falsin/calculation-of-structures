108067b1be17b4afd7361e3b63b790585c6415c0
Falsin <greenldend@gmail.com>
2023-05-22 02:21:33 +0700

    добавил превращение центра тяжести из строки в число

--- src/javascript/addCirclesToSVG.js
+++ src/javascript/addCirclesToSVG.js
@@ -3,15 +3,15 @@ import calcRotateCoords from "./calcRotateCoords";
 export default function createCirclesInSvg(shape) {
   const changedArrayCoords =  shape.coords.map(obj => {
     const rotateCoords = calcRotateCoords(obj, 0, 0, shape.degree);
-    rotateCoords.rotateX += shape.centerX;
-    rotateCoords.rotateY += shape.centerY;
+    rotateCoords.rotateX += +shape.centerX;
+    rotateCoords.rotateY += +shape.centerY;
 
     return {x: +(rotateCoords.rotateX).toFixed(1), y: +(rotateCoords.rotateY).toFixed(1)}
   })
 
   createCirclesInSvg.shapeCollectObj = {
     ...createCirclesInSvg.shapeCollectObj,
-    [shape.uniqid]: changedArrayCoords
+    [shape.id]: changedArrayCoords
   }
 }
 

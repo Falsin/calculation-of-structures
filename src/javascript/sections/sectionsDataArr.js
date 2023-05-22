78303d743b330762c5256ca7908fc95cb0af5de4
Falsin <greenldend@gmail.com>
2023-05-22 22:27:58 +0700

    удалил комментарии

--- src/javascript/sections/sectionsDataArr.js
+++ src/javascript/sections/sectionsDataArr.js
@@ -2,7 +2,6 @@ import { fetchBeams, selectAllBeams } from "../../redux/beamsSlice";
 import { fetchChannels, selectAllChannels } from "../../redux/channelsSlice";
 import { fetchEqualAnglesCorners, selectAllEqualAnglesCorners } from "../../redux/equalAngleCornerSlice";
 import { fetchUnequalAnglesCorners, selectAllUnequalAnglesCorners } from "../../redux/unequalAnglesSlice";
-//import { Beam, Channel, EqualAnglesCorner, UnequalAnglesCorner } from "./Section";
 
 import { createBeam, createChannel, createEqualAnglesCorner, createUnequalAnglesCorner } from "./Sections";
 
@@ -37,35 +36,3 @@ export default [
     defaultActiveCase: 1
   },
 ]
-
-/* export default [
-  {
-    sectionName: "beams",
-    fetch: fetchBeams,
-    selectAllSections: selectAllBeams,
-    classShape: (centerX, centerY, degree, shape) => new Beam(centerX, centerY, degree, shape),
-    sectionNameInRussian: "Двутавр"
-  },
-  {
-    sectionName: "channels",
-    fetch: fetchChannels,
-    selectAllSections: selectAllChannels,
-    classShape: (centerX, centerY, degree, shape) => new Channel(centerX, centerY, degree, shape),
-    sectionNameInRussian: "Швеллер"
-  },
-  {
-    sectionName: "equalAnglesCorners",
-    fetch: fetchEqualAnglesCorners,
-    selectAllSections: selectAllEqualAnglesCorners,
-    classShape: (centerX, centerY, degree, shape) => new EqualAnglesCorner(centerX, centerY, degree, shape),
-    sectionNameInRussian: "Равнополочный уголок"
-  },
-  {
-    sectionName: "unequalAnglesCorners",
-    fetch: fetchUnequalAnglesCorners,
-    selectAllSections: selectAllUnequalAnglesCorners,
-    classShape: (centerX, centerY, degree, shape, activeCase) => new UnequalAnglesCorner(centerX, centerY, degree, shape, activeCase),
-    sectionNameInRussian: "Неравнополочный уголок",
-    defaultActiveCase: 1
-  },
-] */
